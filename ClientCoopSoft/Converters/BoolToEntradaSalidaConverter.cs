832b293f27d586be4cd9a986479b62df8d94150d
maurice920 <maudelab23@gmail.com>
2025-12-18 16:19:46 -0400

    agreagdo tipo entrada o salida

---
+++ ClientCoopSoft/Converters/BoolToEntradaSalidaConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ClientCoopSoft.Converters
+{
+    public class BoolToEntradaSalidaConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool b)
+                return b ? "Entrada" : "Salida";
+
+            return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // No se usa
+            return Binding.DoNothing;
+        }
+    }
+}
